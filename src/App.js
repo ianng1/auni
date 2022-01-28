1d85fa5434581e48937324cc7bafa85842b2600c
Max Pace <maxpace1@icloud.com>
2022-01-28 18:10:19 -0500

    Google sign in works

--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import GoogleLogin from 'react-google-login';
-import { useState } from 'react';
+import { signInWithGoogle } from './Firebase';
+//import { useState } from 'react';
 
 function App() {
-
-  const [loginData, setLoginData] = useState(
-    localStorage.getItem('loginData')
-      ? JSON.parse(localStorage.getItem('loginData'))
-      : null
-  );
-
-  const handleFailure = (result) => {
-    loginData = null
-  };
-
-  const handleLogin = async (googleData) => {
-    const res = await fetch('/api/google-login', {
-      method: 'POST',
-      body: JSON.stringify({
-        token: googleData.tokenId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-
-    const data = await res.json();
-    setLoginData(data);
-    localStorage.setItem('loginData', JSON.stringify(data));
-
-  };
-  const handleLogout = () => {
-    localStorage.removeItem('loginData');
-    setLoginData(null);
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Auni</h1>
-        <div>
-          {
-            loginData ? (
-              <div>
-                <h3> Welcome, {loginData.givenName}</h3>
-                <button onClick={handleLogout}>Sign out</button>
-              </div>
-            )
-              :
-              <GoogleLogin
-                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                buttonText='Log in with Google'
-                onSuccess={handleLogin}
-                onFailure={handleFailure}
-                cookiePolicy={'single_host_origin'}
-              ></GoogleLogin>
-          }
-        </div>
+      <button onClick={signInWithGoogle} class="login-with-google-btn">Sign in with Google</button>
+      <h1>Welcome to Auni, {localStorage.getItem("name")}</h1>
       </header>
     </div>
   );
