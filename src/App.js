0fecee75c7e8c67c640e327a720a4fa8d981a3ae
Max Pace <maxpace1@icloud.com>
2022-02-03 16:39:02 -0500

    Fixed sign in timing, added sign out button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
-import { signInWithGoogle } from './Firebase';
-//import { useState } from 'react';
+import { signInWithGoogle, signOutFirebase } from './Firebase';
+import { useState } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+  const updateUser = () => {
+    setUser({
+      email: localStorage.getItem("email"),
+      name: localStorage.getItem("name"),
+      profilePic: localStorage.getItem("profilePic")
+    });
+  }
+  const signOut = () => {
+    signOutFirebase();
+    localStorage.clear();
+    setUser(null);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-      <button onClick={signInWithGoogle} class="login-with-google-btn">Sign in with Google</button>
-      <h1>Welcome to Auni, {localStorage.getItem("name")}</h1>
+        {
+          user ?
+            <div>
+              <h1>Welcome to Auni, {localStorage.getItem("name")}</h1>
+              <button onClick={signOut} class="sign-out">Sign out</button>
+            </div>
+            :
+            <div>
+              <h1>Welcome to Auni</h1>
+              <button onClick={() => { (signInWithGoogle()).then(() => { console.log("hi!"); updateUser(); }) }} class="login-with-google-btn">Sign in with Google</button>
+            </div>
+        }
       </header>
-    </div>
+    </div >
   );
 }
 
